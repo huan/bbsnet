88adf172121b811e19d154a8bc94f60288174f9e
Huan LI <zixia@zixia.net>
2017-07-09 16:24:41 +0800

    update(main): use main() function

--- index.ts
+++ index.ts
@@ -1,13 +1,18 @@
 import { bbsnet } from './src/bbsnet'
 import { web }    from './src/web'
 
-Promise.all([
-  bbsnet(23),
-  bbsnet(2323),
-  web(),
-]).then(_ => {
-  console.log('bbsnet/web ready')
-}).catch(e => {
-  console.log(e)
-})
 
+async function main(): Promise<void> {
+  try {
+    await Promise.all([
+      bbsnet(23),
+      bbsnet(2323),
+      web(),
+    ])
+    console.log('bbsnet/web ready')
+  } catch(e) {
+    console.log(e)
+  }
+}
+
+main()
