fe65178957adac918d1162db3e40ea635d10ada8
Huan LI <zixia@zixia.net>
2017-08-07 12:12:21 +0800

    add copyright

--- src/web.ts
+++ src/web.ts
@@ -34,12 +34,18 @@ function showHtml(
       <div class="container markdown-body">
         <h1 id="zixia-bbs">zixia bbs</h1>
 
-  <p>波若波罗蜜~服务器Down机啦，怎么办呢？</p>
+  <h3>感谢大家在过去的20年中，对使用 zixia bbs 的喜爱！</h3>
+
+  <p>不过最近服务器Down机啦，怎么办呢？</p>
+
+  <p>波若波罗蜜~</p>
 
   <p><img src="https://raw.githubusercontent.com/chatie/wechaty/master/image/BotQrcode.png" alt="zixia bbs wechat group" /></p>
 
   <p>微信扫描二维码（或直接添加好友 orangiss ），发送暗号”zixia”，加入 zixia Down 机群讨论吧~</p>
 
+
+  <p>1998-2017(c) bbs.zixia.net</p>
 
       </div>
       <script src="/assets/javascript/anchor-js/anchor.min.js"></script>
